5ed081d95ac3a578ba8569acf5d5e92bffe2cc11
NURIM HAN <52388252+hannurim@users.noreply.github.com>
2019-11-20 17:17:52 +0900

    Update detecting.h
    Add Several parameters, Functions(Association, Identification, GetMin), List of p_list and Integer type variable for error count.

--- 2D_LIDAR/detecting.h
+++ 2D_LIDAR/detecting.h
@@ -9,13 +9,20 @@
 #include <geometry_msgs/Twist.h>
 
 #define PI      3.141592
-#define R_VEL   0.2         // rotate velocity
-#define F_VEL   0.2         // forward velocity
+#define R_ANG   1.570796    // Reference angle[rad]
+#define R_DIS   0.4         // Reference distance[m]
+#define Kw      0.6         // Gain of radian velocity
+#define Kv      0.6         // Gain of distance velocity
+#define Kl      0.7         // Weights for updating goal values
+#define R_VEL   0.2
+#define F_VEL   0.2
 
 using namespace std;
 
-void GettingHuman();
-void LostHuman();
+void Association();         // Update to the nearest value within 1.0m of existing value
+void Identification();      // Update data only when a person is detected near 0.4m ahead
+void GettingHuman();        // When a person is detected
+void LostHuman();           // When a person is not detected
 
 geometry_msgs::Twist velOutput;
 
@@ -35,22 +42,56 @@ public:
     Person() { valid = false; goal=make_pair(0,0); }
 };
 
+int GetMin(vector<pair<double, double> > list)
+{
+    double  min     = 99999;
+    int     index   = -1;
+    for (vector<int>::size_type i = 0; i < list.size(); i++)
+    {
+        double dist = sqrt(pow(list[i].first,2) + pow(list[i].second,2));
+        if (min > dist) {
+            min = dist;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int GetMin(vector<pair<int, pair<double, double> > > list)
+{
+    double  min     = 99999;
+    int     index   = -1;
+    for (vector<int>::size_type i = 0; i < list.size(); i++)
+    {
+        double dist = sqrt(pow(list[i].second.first,2) + pow(list[i].second.second,2));
+        if (min > dist) {
+            min = dist;
+            index = i;
+        }
+    }
+    return index;
+}
+
 class Person    p;
 
+// goal<x,y>
+vector<pair<double, double> > p_list;                   // List of people near 0.3m
+
 // index matched clustered data, goal<x,y>
-vector<pair<int, pair<double, double> > > total_p;
+vector<pair<int, pair<double, double> > > total_p;      // List of total people within 1.5m
 
 // width, start<x,y>, finish<x,y>
 vector<pair<double, pair<pair<double, double>, pair<double, double> > > > clustered;    // clustered data
 pair<pair<double, double>, pair<double, double> > c1;
 
 // scan data convert to <x,y>
-vector<pair<double,double> >            raw_data;
+vector<pair<double,double> >            raw_data;       // Coordinated data
 pair<double,double>                     s;
 pair<double,double>                     f;
 pair<int, pair<double,  double> >       p1;
 
 double  dist;
 bool    start_flag = true;
+int     err_cnt = 0;
 
 #endif // DETECTING_H
